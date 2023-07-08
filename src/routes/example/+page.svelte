eac7d1e4a2ab85f9422d35d6c4555bd0ed0e2ca0
Shinichi Okada <okada.shin@gmail.com>
2023-07-08 13:11:27 +0200

    chore: example page

---
+++ src/routes/example/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import * as Icon from '$lib/index.js';
+  import DarkMode from 'flowbite-svelte/DarkMode.svelte';
+  let myClass = 'inline m-1 ';
+</script>
+
+<DarkMode />
+<div class="p-8 text-red-700 dark:text-green-300">
+  <Icon.AddressCardSolid size="md" class={myClass} />
+  <Icon.AdjustmentsHorizontalOutline size="md" class={myClass} />
+  <Icon.AdjustmentsHorizontalSolid size="md" class={myClass} />
+  <Icon.AdjustmentsVerticalOutline size="md" class={myClass} />
+  <Icon.AdjustmentsVerticalSolid size="md" class={myClass} />
+</div>
