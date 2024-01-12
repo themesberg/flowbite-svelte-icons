d4bc7daeabe0964b0344d87c2d59bd4f0b5bcf9b
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-12 15:28:51 +0100

    chore: dependencies update

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import { mdsvex } from 'mdsvex';
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import mdsvexConfig from './mdsvex.config.js';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
