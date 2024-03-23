16dda8ca0ebe9e798101063325d5801d13646921
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-23 16:34:28 +0100

    test: add meta title

--- tests/test.ts
+++ tests/test.ts
@@ -1,51 +1,57 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1, title', async ({ page }) => {
+test('index page has expected h1, meta title', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByRole('heading', { name: 'Flowbite Svelte Icons' })).toBeVisible();
-  await expect(page).toHaveTitle('Flowbite Svelte Icons');
+  await expect(page).toHaveTitle('Accessible SVG Icons - Flowbite Svelte Icons');
 });
 
-test('Outline page has expected h1', async ({ page }) => {
+test('Outline page has expected h1, meta title', async ({ page }) => {
   await page.goto('/outline');
   await expect(page.getByRole('heading', { name: 'Flowbite Svelte Icons: Outline' })).toBeVisible();
+  await expect(page).toHaveTitle('Flowbite Svelte Icons: Outline');
 });
 
-test('Solid page has expected h1', async ({ page }) => {
+test('Solid page has expected h1, meta title', async ({ page }) => {
   await page.goto('/solid');
   await expect(page.getByRole('heading', { name: 'Flowbite Svelte Icons: Solid' })).toBeVisible();
+  await expect(page).toHaveTitle('Flowbite Svelte Icons: Solid');
 });
 
-test('Svelte 4 getting started page has expected h1', async ({ page }) => {
+test('Svelte 4 getting started page has expected h1, meta title', async ({ page }) => {
   await page.goto('/svelte-4/getting-started');
   await expect(page.getByRole('heading', { name: 'Flowbite Svelte Icons: v1' })).toBeVisible();
+  await expect(page).toHaveTitle('Flowbite Svelte Icons v1');
 });
 
-test('Svelte 4 props page has expected h1', async ({ page }) => {
+test('Svelte 4 props page has expected h1, meta title', async ({ page }) => {
   await page.goto('/svelte-4/props');
   await expect(page.getByRole('heading', { name: 'Props: v1' })).toBeVisible();
+  await expect(page).toHaveTitle('Flowbite Svelte Icons v1 Props');
 });
 
-test('Svelte 5 getting started page has expected h1, title', async ({ page }) => {
+test('Svelte 5 getting started page has expected h1, meta title', async ({ page }) => {
   await page.goto('/svelte-5/getting-started');
   await expect(page.getByRole('heading', { name: 'Flowbite Svelte Icons: v2' })).toBeVisible();
-  // expect the page has title in meta tag with Flowbite Svelte Icons
   await expect(page).toHaveTitle('Flowbite Svelte Icons v2');
 });
 
-test('Svelte 5 props page has expected h1', async ({ page }) => {
+test('Svelte 5 props page has expected h1, meta title', async ({ page }) => {
   await page.goto('/svelte-5/props');
   await expect(page.getByRole('heading', { name: 'Props: v2' })).toBeVisible();
+  await expect(page).toHaveTitle('Flowbite Svelte Icons v2 Props');
 });
 
-test('Global icons page has expected h1', async ({ page }) => {
+test('Global icons page has expected h1, meta title', async ({ page }) => {
   await page.goto('/guide/global-icons');
   await expect(
     page.getByRole('heading', { name: 'Setting Global Icon using setContext' })
   ).toBeVisible();
+  await expect(page).toHaveTitle('Flowbite Svelte Icons - Global Icons');
 });
 
-test('Custom default icons props page has expected h1', async ({ page }) => {
+test('Custom default icons props page has expected h1, meta title', async ({ page }) => {
   await page.goto('/guide/custom-icons');
   await expect(page.getByRole('heading', { name: 'Custom Default Icons' })).toBeVisible();
+  await expect(page).toHaveTitle('Flowbite Svelte Icons - Custom Default Icons');
 });
