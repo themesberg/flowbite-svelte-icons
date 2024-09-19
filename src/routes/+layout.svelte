d637819f5b86416285d74de6956c65c6238cd38a
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-09-19 20:34:18 +0200

    docs: asideClass in +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -59,8 +59,8 @@
   {#if urlsToIncludeSwitcherAndSidebar.some((path) => currentUrl.startsWith(path))}
     <Sidebar
       sidebarList={newSidebarList}
-      asideClass="fixed inset-0 z-30 flex-none h-full w-64 lg:static lg:h-auto border-e border-gray-200 dark:border-gray-600 lg:overflow-y-visible lg:pt-0 lg:block hidden"
-      divClass="fixed top-20 px-2 w-60 dark_bg_theme"
+      asideClass="fixed inset-0 z-30 flex-none h-full w-64 lg:static lg:h-auto border-e border-gray-200 dark:border-gray-600 lg:overflow-y-visible lg:pt-0 lg:block hidden bg-gray-50 "
+      divClass="fixed px-2 w-60 dark_bg_theme"
     />
     <div class="relative">
       <OnThisPage {extract} headingSelector="#mainContent > :where(h2, h3)" />
