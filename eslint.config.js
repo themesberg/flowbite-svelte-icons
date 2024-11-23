956435b5c6426f8845e7825db59b65d7a9164df0
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-11-23 16:23:20 +0100

    fix: add ignores .vercel/ to eslint.config.js and .gitignore. fix: add /src/routes/utils/highlight to ignores .prettierignore and eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,6 @@ export default tseslint.config(
     }
   },
   {
-    ignores: ['build/', '.svelte-kit/', 'dist/']
+    ignores: ['src/routes/utils/highlight', '.vercel/', 'build/', '.svelte-kit/', 'dist/']
   }
 );
